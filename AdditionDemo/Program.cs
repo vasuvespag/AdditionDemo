9a875d8497815132fc8471e3eb006466f2716a4a
vasuvespag <vasu.vespag@gmail.com>
2023-02-24 17:06:42 +0530

    First Commit

---
+++ AdditionDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace AddtionDemo
+{
+    public class Addition
+    {
+        public void Add(int val1,int val2) {
+            Console.WriteLine("Inside int method");
+            Console.WriteLine(val1 + val2);
+        }
+
+        public void Add(double val1,double val2)
+        {
+            Console.WriteLine("Inside double method");
+            Console.WriteLine(val1 + val2);
+        }
+
+        public static void Main(string[] args)
+        {
+            Addition a1 = new Addition();
+            a1.Add(5.4, 6.3);
+        }
+    }
+}
